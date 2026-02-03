8c11701c93c056a7b851f6ac2cf7c8bafac39502
eslamhany71-lgtm <eslamhany71@gmail.com>
2026-02-03 16:24:25 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "loan-app-v1.0.1"; // غير الرقم مع أي تحديث
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  clients.claim();
+});
+
+self.addEventListener("fetch", () => {});
+
+self.addEventListener("message", (event) => {
+  if (event.data === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
